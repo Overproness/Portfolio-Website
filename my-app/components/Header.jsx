8b2066de458ccc3f965c38f68ae40718d9bf0aa3
Overproness <mr.overproness@gmail.com>
2024-09-24 00:20:18 +0500

    first commit

---
+++ my-app/components/Header.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import MobileNav from "./MobileNav";
+import Nav from "./Nav";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container flex align-center mx-auto justify-between items-center">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Munt<span className="text-accent">.</span>
+          </h1>
+        </Link>
+
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
